9eb54b7d8758684f985bddab5110145078e5f14e
juan diaz <juanignaciodiaz1994@hotmail.com>
2023-01-23 22:19:35 -0300

    agregando mas funcionalidad js al a tienda

--- index.js
+++ index.js
@@ -13,27 +13,58 @@ const productos = [
     { id: 4, URL:"../imagenes/Camisa.png",nombre: "Camisa Leñador", precio: 1500 },
   ];
   
-  productos.forEach((item) => {
+
+
+const carrito = [];
+
+const agregarAlCarrito = (id) => {
+  const producto = productos.find((item) => item.id === id);
+  carrito.push(producto);
+  const carritoDesplegable = document.querySelector(".carrito-desplegable");
+  const divProducto = document.createElement("div");
+  divProducto.classList.add("carrito-desplegable__detalles-container");
+  divProducto.innerHTML = `
+    <img class ="carrito-desplegable__imagen" src="${producto.URL}" alt="${producto.nombre}">
+    <p class ="carrito-desplegable__producto">${producto.nombre}</p>
+    <p class ="carrito-desplegable__precio">Precio: $${producto.precio}</p>
+  `;
+  document.querySelector(".carrito-desplegable__lista").append(divProducto);
+};
+
+productos.forEach((item) => {
     let section = document.createElement("section");
     section.innerHTML = `
-    <div class="tienda">
-      <h2 class = "noShow">ID: ${item.id}</h2>
-      <img class="tienda__remera1"src=" ${item.URL}"> 
-      <p class="tienda__remeraTitulo">${item.nombre}</p>
-      <b class="tienda__precio">$${item.precio}</b>
-      <button class="tienda__agregar" id="boton${item.id}">Agregar</button>
-    </div>
+      <div class="tienda">
+        <h2 class = "noShow">ID: ${item.id}</h2>
+        <img  class="tienda__remera1"src=" ${item.URL}"> 
+        <p class="tienda__remeraTitulo">${item.nombre}</p>
+        <b class="tienda__precio">$${item.precio}</b>
+        <button class="tienda__agregar" id="boton${item.id}">Agregar</button>
+      </div>
     `;
   
     document.body.append(section);
     let boton = document.getElementById(`boton${item.id}`);
-  
-    boton.addEventListener("click", () => console.log(item));
-  });
+    boton.addEventListener("click", () => agregarAlCarrito(item.id));
+
+});
+
+const eliminarTodoDelCarrito = () => {
+    const carritodesplegable__lista = document.querySelector(".carrito-desplegable__lista");
+    carritodesplegable__lista.innerHTML = ""
+    carrito.length = 0;
+  }
 
   
+  const deleteAllButton = document.getElementById("deleteAll");
+  deleteAllButton.addEventListener("click", eliminarTodoDelCarrito);
+  
 
 
+
+  
+  
+  
 
 /*
 function Producto(nombre, precio, stock){
